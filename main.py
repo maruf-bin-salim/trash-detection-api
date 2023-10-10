6348a9164ac1f16a7ec3005760d82ad6df291537
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-10-10 23:20:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
 from fastai.vision.all import *
-import os
-import base64
-from PIL import Image
 from io import BytesIO
-import json
-
-
+import os
 
 app = FastAPI()
 
@@ -31,48 +26,24 @@ async def root():
     return {"message": "Welcome to the Garbage Classification API!"}
 
 @app.post("/predict")
-async def predict_from_data_url(request: Request):
+async def get_net_image_prediction(file: UploadFile):
+    image_bytes = await file.read()
+
+    if not image_bytes:
+        return {"message": "No image data provided"}
+
     try:
-        data = await request.json()
-        image_data_url = data.get("image_data_url")
-
-        if not image_data_url:
-            raise HTTPException(status_code=422, detail="No image data URL provided")
-
-        # Extract the base64-encoded image data from the Data URL
-        if image_data_url.startswith("data:image/jpeg;base64,"):
-            image_data = image_data_url.split(",")[1]
-            image_bytes = base64.b64decode(image_data)
-
-            if image_bytes is None or len(image_bytes) == 0:
-                raise HTTPException(status_code=422, detail="Couldn't get image bytes from the data URL")
-            
-            bytesToImage = BytesIO(image_bytes)
-            
-            if bytesToImage is None:
-                raise HTTPException(status_code=422, detail="Couldn't convert to bytesIO from the data URL")
-
-            # Convert the image bytes to a PIL Image
-            image = Image.open(bytesToImage)
-
-            if image is None:
-                raise HTTPException(status_code=422, detail="Couldn't convert to image!")
-
-
-            # Perform prediction on the image using your model
-            pred, idx, prob = learn.predict(image)
-
-            classes = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
-            overall_probabilities = [{"class": classes[i], "probability": float(prob[i])} for i in range(len(classes))]
-            overall_probabilities = sorted(overall_probabilities, key=lambda k: k['probability'], reverse=True)
-
-            return {"prediction": {"name": pred, "probability": float(prob[idx])}, "overall_probabilities": overall_probabilities}
-
-        else:
-            raise HTTPException(status_code=422, detail="Invalid image data URL format")
-
+        image = PILImage.create(BytesIO(image_bytes))
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        return {"message": f"Error processing image: {str(e)}"}
+
+    pred, idx, prob = learn.predict(image)
+
+    classes = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
+    overall_probabilities = [{"class": classes[i], "probability": float(prob[i])} for i in range(len(classes))]
+    overall_probabilities = sorted(overall_probabilities, key=lambda k: k['probability'], reverse=True)
+
+    return {"prediction" : {"name": pred, "probability": float(prob[idx])}, "overall_probabilities": overall_probabilities}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
